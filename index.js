1b165315c832c17f4d7095380781386754fa36a8
GavinGoGaming <gavingogaming@gmail.com>
2023-02-22 18:52:36 -0800

    yeeeeeę

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ async function run() {
 
   chat(askGPT, app, askIMG);
 
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
   });
