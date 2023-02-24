fa3e36a4ea1280abc9e9fe8d93c6bbb12ab4f1bb
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2023-02-24 20:15:55 +0000

    Move nav-button-link code to publicjs

--- public/Public.js
+++ public/Public.js
@@ -25,3 +25,10 @@ closeIcon.onclick = () => {
         nav.classList.remove("slide-out");
     }, 250);
 };
+
+var buttons = document.querySelectorAll(".nav ul li:not(.spacer)");
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        window.location.href = btn.dataset.link;
+    });
+});
