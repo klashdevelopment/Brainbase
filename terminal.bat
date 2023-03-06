4b11ddc1207e502083fb5f415723d135a8e080ed
GavinGoGaming <57292172+GavinGoGaming@users.noreply.github.com>
2023-03-06 17:11:57 +0000

    Add style

--- terminal.bat
+++ terminal.bat
@@ -3,7 +3,6 @@
 @K: 
 @cls 
 @echo "-- Gavinfly Terminal --" && echo "  > FLYCTL SETUP" && echo "  > FLYCTL COMPLETE" && echo "  > TERMING" && echo "  >> Enter your commands below, welcome to developer land."
-@pause "Press any key to select!"
 set /p input=A: Run, B: Publish
 IF %input%==A (node .)
 IF %input%==B (fly deploy)
